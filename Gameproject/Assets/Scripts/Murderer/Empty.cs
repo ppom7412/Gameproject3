82a2ede28458593dcafef904f05393cce244f851
DESKTOP-BT9VEM9\Lee <ppom7412@game.hoseo.edu>
2017-11-27 20:57:05 +0900

    살인자 벽통과 수정(검사안해봄) 밝기 수정하는 스크립트 작성

---
+++ Gameproject/Assets/Scripts/Murderer/Empty.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Empty : State<Murderer>
+{
+    public Empty()
+    {
+        name = "Empty";
+    }
+
+    override public void Enter(Murderer murderer)
+    {
+
+    }
+
+    override public void Execute(Murderer murderer)
+    {
+
+    }
+
+    override public void Exit(Murderer murderer)
+    {
+
+    }
+}
