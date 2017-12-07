9b37ecc8832ab32936924bb116308ae8ff936c11
GARO\Garo <kdh96co@gmail.com>
2017-12-07 20:20:18 +0900

    도어 사운드 스크립트

---
+++ Gameproject/Assets/Scripts/SoundWave/DoorSound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSound : MonoBehaviour {
+
+    private SoundWaveData data;
+    private GameObject SoundWaveManager;
+    private GameObject SoundWave;
+
+    // Use this for initialization
+    void Start ()
+    { 
+        data.isActive = 1.0f;
+        data.isEnemy = 0.0f;
+        data.radius = 0.5f;
+        data.subRadius = 0.6f;
+        data.circlePoint = transform.position;
+        SoundWaveManager = GameObject.Find("SoundWaveManager");
+    }
+
+    public void FlagChangePlayer()
+    {
+        data.isEnemy = 0.0f;
+    }
+    public void FlagChangeMurderer()
+    {
+        data.isEnemy = 1.0f;
+    }
+
+
+    public void ActiveSoundWave()
+    {
+        SoundWave = SoundWaveManager.GetComponent<SoundWaveManager>().SearchInactiveObject();
+        SoundWave.GetComponent<SoundWaveSphere>().SetTakeData(data);
+    }
+}
